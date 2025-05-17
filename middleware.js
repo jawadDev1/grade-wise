cfec56df681b3da5ca3a572d59d1ae06238085ce
Jawad Ali <149782239+jawadDev1@users.noreply.github.com>
2025-05-17 09:20:10 +0500

    bugfix: auth middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(req) {
-  const token = req.cookies.get("next-auth.session-token");
+export async function middleware(req) {
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
   if (!token) {
     const signInUrl = new URL("/login", req.url);
